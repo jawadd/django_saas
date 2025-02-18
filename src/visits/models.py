a8905a5f17c7bfa209d09fc7050c615d51eb40fd
Jawad Hussain <jawadduet@gmail.com>
2025-02-18 15:51:11 +0500

    add templates and views

--- src/visits/models.py
+++ src/visits/models.py
@@ -2,5 +2,7 @@ from django.db import models
 
 # Create your models here.
 class PageVisit(models.Model):
-    path = models.TextField(blank=True, null=True) 
-    timestamp = models.DateTimeField(auto_now_add=True) 
+    # db -> table
+    # id -> hidden -> primary key -> autofield -> 1, 2, 3, 4, 5
+    path = models.TextField(blank=True, null=True) # col
+    timestamp = models.DateTimeField(auto_now_add=True) # col
